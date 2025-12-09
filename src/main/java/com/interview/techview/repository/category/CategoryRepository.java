701f413eaaf606fcdaea9ab92a8de602485d7d57
hodu26 <gjdhks1212@gmail.com>
2025-12-10 02:49:45 +0900

    [feat] #3 - 기본적인 Repository 설정
    - User 관련 Repository
    - Report  관련 Repository
    - Question 관련 Repository
    - Category 관련 Repository
    - Skill 관련 Repository

---
+++ src/main/java/com/interview/techview/repository/category/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.interview.techview.repository.category;
+
+import com.interview.techview.domain.category.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
