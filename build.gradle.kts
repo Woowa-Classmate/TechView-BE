ca4363f9752128f2cb3da77d16db4cd49f7e47a2
hodu26 <gjdhks1212@gmail.com>
2025-12-10 02:23:47 +0900

    init: TechView BE

---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+	java
+	id("org.springframework.boot") version "4.0.0"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.interview"
+version = "0.0.1-SNAPSHOT"
+description = "Demo project for Spring Boot"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	runtimeOnly("com.mysql:mysql-connector-j")
+	testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-security-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	// Lombok (자바 스타일 엔티티 작성 시)
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+sourceSets {
+	main {
+		java {
+			// QueryDSL이 생성한 Q클래스를 IDE가 소스 코드로 인식하도록 추가
+			srcDir(layout.buildDirectory.dir("generated/sources/annotationProcessor/java/main"))
+		}
+	}
+}
---
+++ src/test/java/com/interview/techview/TechviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.techview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
