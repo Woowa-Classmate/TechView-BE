4d52c9de0bbeab545c321a7cfd6531a35d918d3c
ChatHongPT <suk924600@naver.com>
2025-12-18 14:24:41 +0900

    [feat] #14 - 게시글 목록 조회에 페이징 기능 추가

--- src/main/java/com/interview/techview/repository/post/PostRepository.java
+++ src/main/java/com/interview/techview/repository/post/PostRepository.java
@@ -1,6 +1,8 @@
 package com.interview.techview.repository.post;
 
 import com.interview.techview.domain.post.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -12,6 +14,9 @@ public interface PostRepository extends JpaRepository<Post, Long>, PostQueryRepo
     @EntityGraph(attributePaths = { "user" })
     @Override
     List<Post> findAll();
+
+    @EntityGraph(attributePaths = { "user" })
+    Page<Post> findAll(Pageable pageable);
 
     @EntityGraph(attributePaths = { "user" })
     @Override
